9345d8d4c2dda72af205125f8c5872073198b39b
Thomas Risberg <trisberg@gopivotal.com>
2013-10-04 15:09:02 -0400

    Cleaning up the main app

--- src/main/java/com/springdeveloper/hadoop/batch/BatchApp.java
+++ src/main/java/com/springdeveloper/hadoop/batch/BatchApp.java
@@ -1,23 +1,14 @@
 package com.springdeveloper.hadoop.batch;
 
 import java.util.Date;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Scanner;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameter;
-import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.beans.factory.BeanInitializationException;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.util.Assert;
 
 public class BatchApp {
 
